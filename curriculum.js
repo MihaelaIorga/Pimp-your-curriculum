ea5bf521d610a9f1457a68b54a124aaab8281642
MihaelaIorga <mihaelalaura.iorga@gmail.com>
2017-09-18 19:59:19 +0200

    js

---
+++ curriculum.js
@@ -0,0 +1,26 @@
+ let subjects = document.querySelector ('.study-prog');
+                let subjectsTemplate = document.querySelector('.mainSubjects').content;
+                let subjectsDataSource = "curriculum.json"
+
+                function getData(link){
+                fetch(link).then(function(response){
+                    return response.json();
+                }).then(function(json){
+                    return show(json);
+                });
+            }
+                function show(json) {
+                    json.forEach(function(subject){
+                        console.log(subject)
+                        let clone = subjectsTemplate.cloneNode(true);
+
+                        clone.querySelector(".t-icon").src = subject.icon.src;
+                        clone.querySelector("h3").textContent = subject.name;
+                        clone.querySelector("h4").textContent = subject.ects;
+                        clone.querySelector(".teach-p").textContent = subject.description;
+
+                        subjects.appendChild(clone);
+
+                    });
+                }
+                getData(subjectsDataSource);
