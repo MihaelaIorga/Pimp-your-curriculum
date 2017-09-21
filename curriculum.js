4bcf39238ec611f64dc4f18c1aeccab854f521f5
MihaelaIorga <mihaelalaura.iorga@gmail.com>
2017-09-21 13:42:57 +0200

    semesters exams! cool

--- curriculum.js
+++ curriculum.js
@@ -41,11 +41,6 @@ function removeActive(link){
             e.target.classList.add('active');
             }
 
-         document.querySelector(".exam").onclick = function(e) {
-            document.querySelectorAll('.navBar a').forEach(removeActive);
-            e.target.classList.add('active');
-            }
-
 
         document.querySelector(".clickintern").onclick = function(e) {
             document.querySelectorAll('.navBar a').forEach(removeActive);
